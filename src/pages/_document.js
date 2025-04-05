5dfd7ea597832c48bf92927a711d10a109b534a2
kapil238 <kapilsanghani111@gmail.com>
2025-04-05 10:31:11 +0530

    Updated favicon and _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Use PNG favicon */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
