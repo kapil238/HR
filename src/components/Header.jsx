8c1095248852dc24ebf639e36fe9e3c80d26b130
kapil238 <kapilsanghani111@gmail.com>
2025-04-10 16:05:22 +0530

    Updated feature.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,7 +88,7 @@ export default function Header() {
                             <button
                                 className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 onClick={() => {
-                                    setProfileOpen(true); // ✅ Open ProfileCard
+                                    setProfileOpen(false); // ✅ Open ProfileCard
                                     setProfileMenuOpen(false);
                                 }}
                             >
